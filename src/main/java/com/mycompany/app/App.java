5f694b63e47cfff34a6eda72b7d87a0834291c53
Mark Lantsberger <mark.lantsberger@sdcoe.net>
2025-02-03 19:16:24 +0000

    Updated JSON Example

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,12 +9,55 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class App {
     public static void main(String[] args) {
-        Gson gson = new Gson();
-        JsonObject obj = new JsonObject();
-        obj.addProperty("key", "value");
-        System.out.println(gson.toJson(obj));
+        try {
+            // API URL
+            String apiUrl = "https://services1.arcgis.com/PCHfdHz4GlDNAhBb/arcgis/rest/services/CalEnviroScreen_4_0_Results_/FeatureServer/0/query?where=1%3D1&outFields=*&f=json";
+
+            // Create HttpClient
+            HttpClient client = HttpClient.newHttpClient();
+
+            // Create HttpRequest
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(apiUrl))
+                    .header("Accept", "application/json")
+                    .GET()
+                    .build();
+
+            // Send request and get response
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Check the response status code
+            if (response.statusCode() != 200) {
+                throw new RuntimeException("HTTP error code: " + response.statusCode());
+            }
+
+            // Parse JSON
+            Gson gson = new Gson();
+            JsonObject jsonResponse = gson.fromJson(response.body(), JsonObject.class);
+
+            // Extract features
+            JsonArray features = jsonResponse.getAsJsonArray("features");
+            for (int i = 0; i < features.size(); i++) {
+                JsonObject feature = features.get(i).getAsJsonObject();
+                JsonObject attributes = feature.getAsJsonObject("attributes");
+
+                // Print keys and values
+                for (String key : attributes.keySet()) {
+                    System.out.println(key + ": " + attributes.get(key));
+                }
+                System.out.println("----------------------------");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+ }
+
